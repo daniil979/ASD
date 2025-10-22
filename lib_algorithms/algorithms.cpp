da89185747a98c0d16fb1b1bced367765e122f66
DANIIL GOLOVAN <golovandaniel@yandex.rugit clone git@github.com:daniil979/ASD.gitssh-keygen -t ed25519 -C _@gmail.comgolovandaniel@yandex.ru>
2025-10-22 11:48:43 +0300

    Обновлена реализация algorithms.cpp
    - [Описание что именно изменили]

--- lib_algorithms/algorithms.cpp
+++ lib_algorithms/algorithms.cpp
@@ -1,25 +1,35 @@
 #include "algorithms.h"
 #include "../stack.h"
-#include <unordered_map>
 
 bool check_brackets(std::string expression) {
-    Stack<char> brackets; 
-    std::unordered_map<char, char> pairs = {
-        {')', '('},
-        {']', '['},
-        {'}', '{'}
-    };
+    Stack<char> brackets;
     
     for (char c : expression) {
         if (c == '(' || c == '[' || c == '{') {
+            
             brackets.push(c);
         } else if (c == ')' || c == ']' || c == '}') {
-            if (brackets.empty() || brackets.top() != pairs[c]) {
+           
+            
+            if (brackets.empty()) {
+                
+                return false;
+            }
+            
+            char top = brackets.top();
+            
+           
+            if (c == ')' && top != '(') {
+                return false;
+            }
+            if (c == ']' && top != '[') {
+                return false;
+            }
+            if (c == '}' && top != '{') {
                 return false;
             }
             brackets.pop();
         }
     }
-    
     return brackets.empty();
 }
