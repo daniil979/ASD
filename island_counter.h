f605a67ddd40f9e3a010b6f5f115ece65926bc4b
daniil979 <golovandaniel@yandex.ru>
2025-11-29 20:46:15 +0300

    Update island_counter.h

--- island_counter.h
+++ island_counter.h
@@ -6,14 +6,11 @@
 
 class IslandCounter {
 public:
-    // Подсчет островов в бинарной матрице
     static int countIslands(const std::vector<std::vector<int>>& matrix);
     
 private:
-    // Проверка валидности координат
     static bool isValid(int row, int col, int rows, int cols);
     
-    // Конвертация 2D координат в 1D индекс для DSU
     static int toIndex(int row, int col, int cols);
 };
 
